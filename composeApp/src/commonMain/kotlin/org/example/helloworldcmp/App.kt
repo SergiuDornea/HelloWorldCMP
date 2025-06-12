10964980660323e212136fde3d67b01cc6525547
Sergiu <sergiu.dornea@p3-grpup.com>
2025-06-12 22:09:27 +0300

    hello world

--- composeApp/src/commonMain/kotlin/org/example/helloworldcmp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/helloworldcmp/App.kt
@@ -2,6 +2,7 @@ package org.example.helloworldcmp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,23 +23,12 @@ import helloworldcmp.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
+        Box(
             modifier = Modifier
-                .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            contentAlignment = Alignment.Center
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            Text("HelloWorld")
         }
     }
 }
